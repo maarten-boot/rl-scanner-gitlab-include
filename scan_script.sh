b3cc593f82067ad75a303dc4602a9c6c35aa772d
Maarten Boot <11140490+maarten-boot@users.noreply.github.com>
2023-10-10 15:49:16 +0200

    proxy goes exclusivly via ENV

--- scan_script.sh
+++ scan_script.sh
@@ -18,10 +18,10 @@
 #   will be deleted before the scan starts.
 #
 # C) We support a optional Proxy configuration,
-# - RL_PROXY_SERVER: optional, string, default ''.
-# - RL_PROXY_PORT: optional, string, default ''.
-# - RL_PROXY_USER: optional, string, default ''.
-# - RL_PROXY_PASSWORD: optional, string, default ''.
+# - RLSECURE_PROXY_SERVER: optional, string, default ''.
+# - RLSECURE_PROXY_PORT: optional, string, default ''.
+# - RLSECURE_PROXY_USER: optional, string, default ''.
+# - RLSECURE_PROXY_PASSWORD: optional, string, default ''.
 #
 # D) If we have a local runner configured using the docker gitlab runner procedure,
 #    we can store scan results local and optionally perform diff scan with:
@@ -35,7 +35,7 @@
 set +e # we handle errors ourselves in this script
 do_verbose()
 {
-    cat <<!
+  cat <<!
 REPORT_PATH:              ${REPORT_PATH:-No path specified}
 PACKAGE_PATH:             ${PACKAGE_PATH:-No path specified}
 MY_ARTIFACT_TO_SCAN:      ${MY_ARTIFACT_TO_SCAN:-No artifact specified}
@@ -50,181 +50,158 @@ RLSECURE_PROXY_PASSWORD:  ${RLSECURE_PROXY_PASSWORD:-No proxy pass was provided}
 }
 fatal()
 {
-    local msg="$1"
-    echo "${msg}" >&2
-    DESCRIPTION="${msg}"
-    STATUS="error"
-    exit 101
+  local msg="$1"
+  echo "${msg}" >&2
+  DESCRIPTION="${msg}"
+  STATUS="error"
+  exit 101
 }
 verify_licence()
 {
-    [ -z "${RLSECURE_SITE_KEY}" ] && {
-        msg="we require 'RSECURE_SITE_KEY' to exist as a env variable"
-        fatal "${msg}"
-    }
-    [ -z "${RLSECURE_ENCODED_LICENSE}" ] && {
-        msg="we require 'RLSECURE_ENCODED_LICENSE' to exist as a env variable"
-        fatal "${msg}"
-    }
+  [ -z "${RLSECURE_SITE_KEY}" ] && {
+      msg="we require 'RSECURE_SITE_KEY' to exist as a env variable"
+      fatal "${msg}"
+  }
+  [ -z "${RLSECURE_ENCODED_LICENSE}" ] && {
+      msg="we require 'RLSECURE_ENCODED_LICENSE' to exist as a env variable"
+      fatal "${msg}"
+  }
 }
 prep_report()
 {
-    if [ -d "${REPORT_PATH}" ]
-    then
-        if rmdir "${REPORT_PATH}"
-        then
-            :
-        else
-            msg="FATAL: your current REPORT_PATH is not empty"
-            DESCRIPTION="${msg}"
-            STATUS="error"
-            exit 101
-        fi
-    fi
-    mkdir -p "${REPORT_PATH}"
+  if [ -d "${REPORT_PATH}" ]
+  then
+      if rmdir "${REPORT_PATH}"
+      then
+          :
+      else
+          msg="FATAL: your current REPORT_PATH is not empty"
+          DESCRIPTION="${msg}"
+          STATUS="error"
+          exit 101
+      fi
+  fi
+  mkdir -p "${REPORT_PATH}"
 }
 verify_paths()
 {
-    [ -z "${REPORT_PATH}" ] && {
-        msg="FATAL: 'REPORT_PATH' is not specified"
-        fatal "$msg"
-    }
-    [ -f "./${PACKAGE_PATH}/${MY_ARTIFACT_TO_SCAN}" ] || {
-        msg="missing artifact to scan: no file found at: ${PACKAGE_PATH}/${MY_ARTIFACT_TO_SCAN}"
-        fatal "$msg"
-    }
+  [ -z "${REPORT_PATH}" ] && {
+      msg="FATAL: 'REPORT_PATH' is not specified"
+      fatal "$msg"
+  }
+  [ -f "./${PACKAGE_PATH}/${MY_ARTIFACT_TO_SCAN}" ] || {
+      msg="missing artifact to scan: no file found at: ${PACKAGE_PATH}/${MY_ARTIFACT_TO_SCAN}"
+      fatal "$msg"
+  }
 }
 extractProjectFromPackageUrl()
 {
-    echo "${RL_PACKAGE_URL}" |
-    awk '{
-        sub(/@.*/,"")       # remove the @Version part
-        split($0, a , "/")  # we expect $Project/$Package
-        print a[1]          # print Project
-    }'
+  echo "${RL_PACKAGE_URL}" |
+  awk '{
+      sub(/@.*/,"")       # remove the @Version part
+      split($0, a , "/")  # we expect $Project/$Package
+      print a[1]          # print Project
+  }'
 }
 extractPackageFromPackageUrl()
 {
-    echo "${RL_PACKAGE_URL}" |
-    awk '{
-        sub(/@.*/,"")       # remove the @Version part
-        split($0, a , "/")  # we expect $Project/$Package
-        print a[2]          # print Package
-    }'
+  echo "${RL_PACKAGE_URL}" |
+  awk '{
+      sub(/@.*/,"")       # remove the @Version part
+      split($0, a , "/")  # we expect $Project/$Package
+      print a[2]          # print Package
+  }'
 }
 makeDiffWith()
 {
-    DIFF_WITH=""
-    if [ -z "$RL_STORE" ]
-    then
-        return
-    fi
-    if [ -z "${RL_PACKAGE_URL}" ]
-    then
-        return
-    fi
-    if [ -z "${RL_DIFF_WITH}" ]
-    then
-        return
-    fi
-    # Split the package URL and find Project and Package
-    Project=$( extractProjectFromPackageUrl )
-    Package=$( extractPackageFromPackageUrl )
-    if [ ! -d "$RL_STORE/.rl-secure/projects/${Project}/packages/${Package}/versions/${RL_DIFF_WITH}" ]
-    then
-        echo "That version has not been scanned yet: ${RL_DIFF_WITH} in Project: ${Project} and Package: ${Package}"
-        echo "No diff scan will be executed, only ${RL_PACKAGE_URL} will be scanned"
-        return
-    fi
-    DIFF_WITH="--diff-with=${RL_DIFF_WITH}"
-}
-prep_proxy_data()
-{
-    PROXY_DATA=""
-    if [ ! -z "${RLSECURE_PROXY_SERVER}" ]
-    then
-        PROXY_DATA="${PROXY_DATA} -e RLSECURE_PROXY_SERVER=${RLSECURE_PROXY_SERVER}"
-    fi
-    if [ ! -z "${RLSECURE_PROXY_PORT}" ]
-    then
-        PROXY_DATA="${PROXY_DATA} -e RLSECURE_PROXY_PORT=${RLSECURE_PROXY_PORT}"
-    fi
-    if [ ! -z "${RLSECURE_PROXY_USER}" ]
-    then
-        PROXY_DATA="${PROXY_DATA} -e RLSECURE_PROXY_USER=${RLSECURE_PROXY_USER}"
-    fi
-    if [ ! -z "${RLSECURE_PROXY_PASSWORD}" ]
-    then
-        PROXY_DATA="${PROXY_DATA} -e RLSECURE_PROXY_PASSWORD=${RLSECURE_PROXY_PASSWORD}"
-    fi
+  DIFF_WITH=""
+  if [ -z "$RL_STORE" ]
+  then
+      return
+  fi
+  if [ -z "${RL_PACKAGE_URL}" ]
+  then
+      return
+  fi
+  if [ -z "${RL_DIFF_WITH}" ]
+  then
+      return
+  fi
+  # Split the package URL and find Project and Package
+  Project=$( extractProjectFromPackageUrl )
+  Package=$( extractPackageFromPackageUrl )
+  if [ ! -d "$RL_STORE/.rl-secure/projects/${Project}/packages/${Package}/versions/${RL_DIFF_WITH}" ]
+  then
+      echo "That version has not been scanned yet: ${RL_DIFF_WITH} in Project: ${Project} and Package: ${Package}"
+      echo "No diff scan will be executed, only ${RL_PACKAGE_URL} will be scanned"
+      return
+  fi
+  DIFF_WITH="--diff-with=${RL_DIFF_WITH}"
 }
 run_scan_nostore()
 {
-    rl-scan \
-        ${PROXY_DATA} \
-        --package-path="./${PACKAGE_PATH}/${MY_ARTIFACT_TO_SCAN}" \
-        --report-path="${REPORT_PATH}" \
-        --report-format=all 1>1 2>2
-    RR=$?
+  rl-scan \
+      --package-path="./${PACKAGE_PATH}/${MY_ARTIFACT_TO_SCAN}" \
+      --report-path="${REPORT_PATH}" \
+      --report-format=all 1>1 2>2
+  RR=$?
 }
 run_scan_withstore()
 {
-    rl-scan \
-        ${PROXY_DATA} \
-        --rl-store="${RL_STORE}" \
-        --purl="${RL_PACKAGE_URL}" \
-        --replace \
-        --package-path="./${PACKAGE_PATH}/${MY_ARTIFACT_TO_SCAN}" \
-        --report-path="${REPORT_PATH}" \
-        --report-format=all \
-        ${DIFF_WITH} 1>1 2>2
-    RR=$?
+  rl-scan \
+      --rl-store="${RL_STORE}" \
+      --purl="${RL_PACKAGE_URL}" \
+      --replace \
+      --package-path="./${PACKAGE_PATH}/${MY_ARTIFACT_TO_SCAN}" \
+      --report-path="${REPORT_PATH}" \
+      --report-format=all \
+      ${DIFF_WITH} 1>1 2>2
+  RR=$?
 }
 get_scan_result_or_fail()
 {
-    DESCRIPTION=$( grep 'Scan result:' 1 )
-    [ -z "${DESCRIPTION}" ] && {
-        # show stderr of the scan command on error
-        echo "# StdErr:"
-        cat 2
-        echo
-        echo "# StdOut:"
-        cat 1
-        echo
-        msg="rl-scan exit with: $RR"
-        fatal "${msg}"
-    }
+  DESCRIPTION=$( grep 'Scan result:' 1 )
+  [ -z "${DESCRIPTION}" ] && {
+      # show stderr of the scan command on error
+      echo "# StdErr:"
+      cat 2
+      echo
+      echo "# StdOut:"
+      cat 1
+      echo
+      msg="rl-scan exit with: $RR"
+      fatal "${msg}"
+  }
 }
 process_scan_result()
 {
-    echo "# StdOut:"
-    cat 1
-    echo
-    STATUS="failed"
-    [ "${RR}" == "0" ] && {
-        STATUS="success"
-    }
-    echo "Status: ${STATUS}; ${DESCRIPTION}"
-    exit ${RR}
+  echo "# StdOut:"
+  cat 1
+  echo
+  STATUS="failed"
+  [ "${RR}" == "0" ] && {
+      STATUS="success"
+  }
+  echo "Status: ${STATUS}; ${DESCRIPTION}"
+  exit ${RR}
 }
 main()
 {
-    if [ ! -z "${RL_VERBOSE}" ]
-    then
-        do_verbose
-    fi
-    verify_licence
-    verify_paths
-    prep_report
-    prep_proxy_data
-    makeDiffWith
-    if [ -z "${RL_STORE}" ]
-    then
-        run_scan_nostore
-    else
-        run_scan_withstore
-    fi
-    get_scan_result_or_fail
-    process_scan_result
+  if [ ! -z "${RL_VERBOSE}" ]
+  then
+      do_verbose
+  fi
+  verify_licence
+  verify_paths
+  prep_report
+  makeDiffWith
+  if [ -z "${RL_STORE}" ]
+  then
+      run_scan_nostore
+  else
+      run_scan_withstore
+  fi
+  get_scan_result_or_fail
+  process_scan_result
 }
 main $*
